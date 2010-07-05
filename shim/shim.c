b8849e164c394ec1642556a5ad8f2e7462f3d905
Nick Black <dank@qemfd.net>
2010-07-05 04:58:45 -0400

    begin decoding CUDA ioctl 0x2a (invoke GPU method)

--- shim/shim.c
+++ shim/shim.c
@@ -74,11 +74,59 @@ void *mmap(void *addr,size_t len,int prot,int flags,int fd,off_t off){
 	return mmap64(addr,len,prot,flags,fd,off);
 }
 
+static void
+dump_mem(const uint32_t *dat,size_t s){
+	size_t z;
+
+	for(z = 0 ; z < s ; z += 4){
+		printf("\x1b[1m");
+		if(z % 16 == 0 && z){
+			printf("0x%04x\t\t\t\t",z);
+		}
+		if(dat[z / 4]){
+			printf("\x1b[32m");
+		}
+		printf("0x%08x ",dat[z / 4]);
+		printf("\x1b[0m");
+		if(z % 16 == 12){
+			printf("\n");
+		}
+	}
+	if(z % 16){
+		printf("\n");
+	}
+}
+
+static void
+decode_gpucall_pre(const uint32_t *dat,size_t s,size_t *ps){
+	if(s != 32){
+		fprintf(stderr,"Invalid argument for GPU invocation (%zub)\n",s);
+		return;
+	}
+	printf("GPU method 0x%08x:%08x\t",dat[1],dat[2]);
+	*ps = ((uint64_t)dat[7] << 32) + dat[6];
+	dump_mem(((uint64_t)dat[5] << 32) + dat[4],*ps);
+}
+
+static void
+decode_gpucall_post(const uint32_t *dat,size_t s,size_t ps){
+	if(s != 32){
+		fprintf(stderr,"Invalid argument for GPU invocation (%zub)\n",s);
+		return;
+	}
+	printf("GPU method 0x%08x:%08x\t",dat[1],dat[2]);
+	if(ps != ((uint64_t)dat[7] << 32) + dat[6]){
+		printf("**************MODIFICATION FROM CALL\n");
+	}
+	dump_mem(((uint64_t)dat[5] << 32) + dat[4],ps);
+}
+
 int ioctl(int fd,int req,uintptr_t op){//,unsigned o1,unsigned o2){
 	static int (*shim_ioctl)(int,int,uintptr_t,int,int);
 	static int (*shim_ioctl3)(int,int,uintptr_t);
 	const uint32_t *dat = (const uint32_t *)op;
-	int r,s,z;
+	size_t *gpus;
+	int r,s;
 
 	if(shim_ioctl == NULL){
 		const char *msg;
@@ -107,44 +155,22 @@ int ioctl(int fd,int req,uintptr_t op){//,unsigned o1,unsigned o2){
 	}
 	s = (req >> 16u) & 0x3fff;
 	printf("ioctl %x, %d-byte param, fd %d\t",req & 0xff,s,fd);
-	for(z = 0 ; z < s ; z += 4){
-		printf("\x1b[1m");
-		if(z % 16 == 0 && z){
-			printf("0x%04x\t\t\t\t",z);
-		}
-		if(dat[z / 4]){
-			printf("\x1b[32m");
-		}
-		printf("0x%08x ",dat[z / 4]);
-		printf("\x1b[0m");
-		if(z % 16 == 12){
-			printf("\n");
-		}
+	dump_mem(dat,s);
+
+	if((req & 0xffu) == 0x2au){
+		decode_gpucall_pre(dat,s,&gpus);
 	}
-	if(z % 16){
-		printf("\n");
-	}
+
 	r = shim_ioctl3(fd,req,op);
 	printf("\x1b[1m\x1b[34mRESULT: %d\x1b[0m\t\t\t",r);
 	if(r == 0){
-		for(z = 0 ; z < s ; z += 4){
-			printf("\x1b[1m");
-			if(z % 16 == 0 && z){
-				printf("0x%04x\t\t\t\t",z);
-			}
-			if(dat[z / 4]){
-				printf("\x1b[32m");
-			}
-			printf("0x%08x ",dat[z / 4]);
-			printf("\x1b[0m");
-			if(z % 16 == 12){
-				printf("\n");
-			}
-		}
-		if(z % 16){
-			printf("\n");
-		}
+		dump_mem(dat,s);
 	}
+
+	if((req & 0xffu) == 0x2a){
+		decode_gpucall_post(dat,s,gpus);
+	}
+
 	printf("\n");
 	return r;
 }
