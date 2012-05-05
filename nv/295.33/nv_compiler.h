98bc46ffd7c7a24c732da7defab7a354fd4ba4f8
nick black <dank@qemfd.net>
2012-05-04 21:51:15 -0400

    bump to 295.40

---
+++ nv/295.33/nv_compiler.h
@@ -0,0 +1,1 @@
+#define NV_COMPILER "gcc version 4.6.3 (Debian 4.6.3-2) "
---
+++ nv/295.40/nv-mempool.c
@@ -0,0 +1,20 @@
+/* _NVRM_COPYRIGHT_BEGIN_
+ *
+ * Copyright 2011 by NVIDIA Corporation.  All rights reserved.  All
+ * information contained herein is proprietary and confidential to NVIDIA
+ * Corporation.  Any use, reproduction, or disclosure without the written
+ * permission of NVIDIA Corporation is prohibited.
+ *
+ * _NVRM_COPYRIGHT_END_
+ */
+
+#define  __NO_VERSION__
+#include "nv-misc.h"
+
+#include "os-interface.h"
+#include "nv-linux.h"
+
+int nv_mem_pool_create(void)
+{
+    return 0;
+}
